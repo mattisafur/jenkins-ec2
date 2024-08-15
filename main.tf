47897c615ba04f46e3c5573925aac89af85417aa
mattisafur <46404761+mattisafur@users.noreply.github.com>
2024-08-15 15:26:52 +0300

    added outbound rule for all ports/locations

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "aws_security_group" "jenkins" {
   name = "jenkins test instance"
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_jenkins_web" {
+resource "aws_vpc_security_group_ingress_rule" "allow_inbound_jenkins_web" {
   security_group_id = aws_security_group.jenkins.id
   cidr_ipv4         = var.web_panel_access_cidr_ipv4
   from_port         = 8080
@@ -34,12 +34,18 @@ resource "aws_vpc_security_group_ingress_rule" "allow_jenkins_web" {
   ip_protocol       = "TCP"
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_jenkins_ssh" {
+resource "aws_vpc_security_group_ingress_rule" "allow_inbound_ssh" {
   security_group_id = aws_security_group.jenkins.id
   cidr_ipv4         = var.ssh_access_cidr_ipv4
   from_port         = 22
   to_port           = 22
   ip_protocol       = "TCP"
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_outbound_all" {
+  security_group_id = aws_security_group.jenkins.id
+  cidr_ipv4 = "0.0.0.0/0"
+  ip_protocol = -1
 }
 
 resource "aws_instance" "jenkins" {
